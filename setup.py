3fb184dcc829d85fefdc42e0ae0124aa302eae1a
Ankit Sachan <>
2021-12-15 17:06:10 +0530

    no message

--- setup.py
+++ setup.py
@@ -1,18 +1,15 @@
 import setuptools
-
+from setuptools import setup, find_packages
 # with open("README.md", "r", encoding="utf-8") as fh:
 #     long_description = fh.read()
 
-setuptools.setup(
-    name="ankit_test_pa",
-    version="0.0.1",
-    author="Ankit",
-    author_email="author@example.com",
-    description="A small example package",
-    long_description='long_description',
-    long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+setup(
+    name='ai_ankit_processing',
+    package_dir={'': 'src'},
+    packages=find_packages(where='src'),
+    python_requires='>=3.5, <4',
+    install_requires=[
+    ],
 
-    packages=['src'],
-    python_requires=">=3.6",
+
 )
